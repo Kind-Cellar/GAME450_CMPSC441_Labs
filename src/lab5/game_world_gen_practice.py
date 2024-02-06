07493c5badf5045759fe68ef4b9e7a1c6799183b
Dustin.Knowlton <dlk5497@psu.edu>
2024-02-06 11:39:14 -0500

    Commit for Lab5

--- src/lab5/game_world_gen_practice.py
+++ src/lab5/game_world_gen_practice.py
@@ -20,6 +20,7 @@ import pygame
 import random
 import numpy as np
 from landscape import get_landscape
+from bresenham import bresenham
 
 from pathlib import Path
 sys.path.append(str((Path(__file__)/'..'/'..').resolve().absolute()))
@@ -29,23 +30,49 @@ from lab2.cities_n_routes import get_randomly_spread_cities, get_routes
 # TODO: Demo blittable surface helper function
 
 ''' Create helper functions here '''
+def drawCities(screen, city_locations_dict):
+    ''' draw cities '''
+    for city in city_locations_dict:
+        val = city_locations_dict[city]
+        pygame.draw.rect(screen, (0,0,0), [val[0], val[1], 10,10], 0)
+
+def drawRoutes(screen, routes, city_locations_dict):
+    ''' draw first 10 routes '''
+    for route in routes:
+        firstCity = route[0]
+        secondCity = route[1]
+        firstCityLocation = city_locations_dict[firstCity]
+        secondCityLocation = city_locations_dict[secondCity]
+        firstCityX, firstCityY = firstCityLocation[0], firstCityLocation[1]
+        secondCityX, secondCityY = secondCityLocation[0], secondCityLocation[1]
+        pygame.draw.line(screen, (0,0,0), [firstCityX, firstCityY], [secondCityX, secondCityY], 2)
+
+def createGameSurface(size):
+    screen = pygame.display.set_mode(size)
+    landscape = get_landscape(size)
+    print("Created a landscape of size", landscape.shape)
+    pygame_surface = pygame.surfarray.make_surface(landscape[:, :, :3])
+    return screen,pygame_surface
+
+def setupCitiesAndRoutes(size, city_names):
+    city_locations = [] 
+    routes = []
+
+    city_locations = get_randomly_spread_cities(size, len(city_names))
+    routes = get_routes(city_names)
+    return city_locations,routes
 
 if __name__ == "__main__":
     pygame.init()
     size = width, height = 640, 480
     black = 1, 1, 1
 
-    screen = pygame.display.set_mode(size)
-    landscape = get_landscape(size)
-    print("Created a landscape of size", landscape.shape)
-    pygame_surface = pygame.surfarray.make_surface(landscape[:, :, :3]) 
+    screen, pygame_surface = createGameSurface(size) 
 
     city_names = ['Morkomasto', 'Morathrad', 'Eregailin', 'Corathrad', 'Eregarta',
                   'Numensari', 'Rhunkadi', 'Londathrad', 'Baernlad', 'Forthyr']
-    city_locations = [] 
-    routes = []
-
-    ''' Setup cities and routes in here'''
+    
+    city_locations, routes = setupCitiesAndRoutes(size, city_names)
 
     city_locations_dict = {name: location for name, location in zip(city_names, city_locations)}
     random.shuffle(routes)
@@ -59,8 +86,8 @@ if __name__ == "__main__":
         screen.fill(black)
         screen.blit(pygame_surface, (0, 0))
 
-        ''' draw cities '''
-
-        ''' draw first 10 routes '''
+        drawCities(screen, city_locations_dict)
+        
+        drawRoutes(screen, routes, city_locations_dict)
 
         pygame.display.flip()
--- src/lab5/landscape.py
+++ src/lab5/landscape.py
@@ -5,6 +5,26 @@ import numpy as np
 def get_elevation(size):
     xpix, ypix = size
     elevation = np.array([])
+    noise = PerlinNoise(octaves=10, seed=1)
+    noise2 = PerlinNoise(octaves=100, seed=2)
+    #noise3 = PerlinNoise(octaves=200, seed=3)
+
+
+    xpix, ypix = size[0], size[1]
+    #elevation = np.array([[noise([i / xpix, j/ypix]) for j in range(xpix)] for i in range (ypix)])
+
+    result = []
+    for i in range(xpix):
+        row = []
+        for j in range(ypix):
+            val = noise([i/xpix, j/ypix])
+            val += .5 * noise2([i/xpix, j/ypix])
+            #val += .25 * noise3([i/xpix, j/ypix])
+
+            row.append(val)
+        result.append(row)
+
+    elevation = np.array(result)
     '''Play around with perlin noise to get a better looking landscape (This is required for the lab)'''
 
     return elevation
